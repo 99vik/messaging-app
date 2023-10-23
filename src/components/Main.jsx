fdaf2768005c184edf05234cd3737e7f86fe61e7
Viktor Miric <viktorm47@gmail.com>
2023-10-23 20:53:40 +0200

    Home page styling

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,34 @@
 import FriendsDisplay from './FriendsDisplay';
 import ChatDisplay from './ChatDisplay';
 import GroupsDisplay from './GroupsDisplay';
+import { AuthorizationDataContext } from '../scripts/AuthorizationDataContext';
+import { useContext, useEffect, useState } from 'react';
+import { getCurrentUser } from '../scripts/ProfileApiCalls';
 
 function Main({ mainDisplay }) {
+  function Home() {
+    const { authorizationData } = useContext(AuthorizationDataContext);
+    const [username, setUsername] = useState(null);
+
+    useEffect(() => {
+      async function getData() {
+        const response = await getCurrentUser(authorizationData.token);
+        setUsername(response.username);
+      }
+      getData();
+    }, []);
+
+    return (
+      <div className="h-full w-full flex flex-col justify-center dark:home-background-animated-dark home-background-animated">
+        {username && (
+          <>
+            <p className="ml-[35%] font-bold mb-2 text-3xl">Welcome,</p>
+            <p className="ml-[35%] font-bold text-6xl">{username}</p>
+          </>
+        )}
+      </div>
+    );
+  }
   function switchResult() {
     switch (mainDisplay[0]) {
       case 'friends':
@@ -12,7 +38,7 @@ function Main({ mainDisplay }) {
       case 'groups':
         return <GroupsDisplay />;
       default:
-        return <h1>Home</h1>;
+        return <Home />;
     }
   }
 
