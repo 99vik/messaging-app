c9dded54a2a52adf7bbf579de4b508ad10abf386
Viktor Miric <viktorm47@gmail.com>
2023-10-26 18:33:18 +0200

    Chat options popup with closing on outside click

--- src/components/ChatDisplay.jsx
+++ src/components/ChatDisplay.jsx
@@ -10,8 +10,11 @@ TimeAgo.addDefaultLocale(en);
 function ChatDisplay({ chat }) {
   const [userProfile, setUserProfile] = useState(null);
   const [messages, setMessages] = useState(null);
+  const [chatOptions, setChatOptions] = useState(false);
   const [loader, setLoader] = useState(true);
   const { authorizationData } = useContext(AuthorizationDataContext);
+  const optionsBtnRef = useRef(0);
+
   useEffect(() => {
     async function getMessages() {
       const response = await GetChatMessages(chat.id, authorizationData.token);
@@ -25,6 +28,48 @@ function ChatDisplay({ chat }) {
     setLoader(true);
     getMessages();
   }, [chat]);
+
+  function toggleOptions() {
+    setChatOptions(!chatOptions);
+  }
+
+  function Options({ optionsBtnRef, close }) {
+    const optionsDiv = useRef(0);
+
+    useEffect(() => {
+      function listenForClick(e) {
+        if (
+          e.target === optionsDiv.current ||
+          Array.from(optionsDiv.current.children).includes(e.target) ||
+          e.target === optionsBtnRef.current ||
+          e.target.parentNode === optionsBtnRef.current
+        ) {
+          return;
+        }
+        close();
+      }
+      document.addEventListener('click', listenForClick);
+
+      return () => document.removeEventListener('click', listenForClick);
+    }, []);
+
+    return (
+      <div
+        ref={optionsDiv}
+        className="absolute flex flex-col gap-2 w-[220px] z-50 appear-fast bg-white dark:bg-slate-700 px-3 py-1 pb-2 -left-[220px] rounded-lg border border-sky-400 dark:border-sky-900 dark:shadow-[0_0_6px_3px_rgba(0,160,255,0.1)] shadow-[0_0_6px_3px_rgba(0,160,255,0.2)]"
+      >
+        <p className="text-sm text-neutral-500 dark:text-neutral-300 font-semibold">
+          Options
+        </p>
+        <button className="bg-sky-500 w-fit hover:bg-sky-700 transition py-1 px-2 rounded-lg text-white">
+          See all participants
+        </button>
+        <button className="bg-red-500 w-fit hover:bg-red-700 dark:bg-red-700 dark:hover:bg-red-900 transition py-1 px-2 rounded-lg text-white">
+          Leave chat
+        </button>
+      </div>
+    );
+  }
 
   function Message({ message }) {
     const timeAgo = new TimeAgo('en-US');
@@ -161,16 +206,25 @@ function ChatDisplay({ chat }) {
         <p className="text-3xl text-neutral-500 dark:text-neutral-300">
           {chat.name}
         </p>
-        <button>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 24 24"
-            className="w-9 fill-sky-500 hover:fill-sky-700 transition"
-          >
-            <title>Settings</title>
-            <path d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
-          </svg>
-        </button>
+        <div className="relative">
+          {chatOptions && (
+            <Options
+              optionsBtnRef={optionsBtnRef}
+              close={() => setChatOptions(false)}
+            />
+          )}
+          <button onClick={toggleOptions}>
+            <svg
+              ref={optionsBtnRef}
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              className="w-9 fill-sky-500 hover:fill-sky-700 transition"
+            >
+              <title>Settings</title>
+              <path d="M12,15.5A3.5,3.5 0 0,1 8.5,12A3.5,3.5 0 0,1 12,8.5A3.5,3.5 0 0,1 15.5,12A3.5,3.5 0 0,1 12,15.5M19.43,12.97C19.47,12.65 19.5,12.33 19.5,12C19.5,11.67 19.47,11.34 19.43,11L21.54,9.37C21.73,9.22 21.78,8.95 21.66,8.73L19.66,5.27C19.54,5.05 19.27,4.96 19.05,5.05L16.56,6.05C16.04,5.66 15.5,5.32 14.87,5.07L14.5,2.42C14.46,2.18 14.25,2 14,2H10C9.75,2 9.54,2.18 9.5,2.42L9.13,5.07C8.5,5.32 7.96,5.66 7.44,6.05L4.95,5.05C4.73,4.96 4.46,5.05 4.34,5.27L2.34,8.73C2.21,8.95 2.27,9.22 2.46,9.37L4.57,11C4.53,11.34 4.5,11.67 4.5,12C4.5,12.33 4.53,12.65 4.57,12.97L2.46,14.63C2.27,14.78 2.21,15.05 2.34,15.27L4.34,18.73C4.46,18.95 4.73,19.03 4.95,18.95L7.44,17.94C7.96,18.34 8.5,18.68 9.13,18.93L9.5,21.58C9.54,21.82 9.75,22 10,22H14C14.25,22 14.46,21.82 14.5,21.58L14.87,18.93C15.5,18.67 16.04,18.34 16.56,17.94L19.05,18.95C19.27,19.03 19.54,18.95 19.66,18.73L21.66,15.27C21.78,15.05 21.73,14.78 21.54,14.63L19.43,12.97Z" />
+            </svg>
+          </button>
+        </div>
       </div>
       <div>
         <div className="h-[1px] w-full bg-slate-300 dark:bg-slate-600"></div>
