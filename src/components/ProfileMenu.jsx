f2cfd181e9ecfebf8fb9ffa8716c8492731beb89
Viktor Miric <viktorm47@gmail.com>
2023-10-27 17:36:48 +0200

    Fetching profile picture on user profile with animations

--- src/components/ProfileMenu.jsx
+++ src/components/ProfileMenu.jsx
@@ -136,13 +136,26 @@ function ProfileMenu({ toggleProfileMenu }) {
                     </button>
                   </>
                 ) : (
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    viewBox="0 0 24 24"
-                    className="w-[200px] fill-sky-500"
-                  >
-                    <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
-                  </svg>
+                  <>
+                    {data.image ? (
+                      <>
+                        <div className="bg-neutral-200 -z-10 animate-pulse transition rounded-full top-[50%] left-[50%] -translate-x-[50%] -translate-y-[50%] h-[167px] w-[167px] absolute"></div>
+                        <img
+                          src={data.image}
+                          className="w-[167px] h-[167px] rounded-full my-[16.5px]"
+                          alt="profile picture"
+                        />
+                      </>
+                    ) : (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        className="w-[200px] fill-sky-500"
+                      >
+                        <path d="M12,19.2C9.5,19.2 7.29,17.92 6,16C6.03,14 10,12.9 12,12.9C14,12.9 17.97,14 18,16C16.71,17.92 14.5,19.2 12,19.2M12,5A3,3 0 0,1 15,8A3,3 0 0,1 12,11A3,3 0 0,1 9,8A3,3 0 0,1 12,5M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12C22,6.47 17.5,2 12,2Z" />
+                      </svg>
+                    )}
+                  </>
                 )}
               </div>
               <div className="w-full px-10">
