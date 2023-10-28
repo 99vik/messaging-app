dc6c677782ec7e1cb6db4e4f239cc8504d0e105d
Viktor Miric <viktorm47@gmail.com>
2023-10-28 16:08:31 +0200

    Adding user to chat styling with loading and added disabling button

--- src/components/AddParticipantPopup.jsx
+++ src/components/AddParticipantPopup.jsx
@@ -23,11 +23,21 @@ function AddParticipantPopup({ chat, close }) {
     const displayedUsers = users.map((user) => (
       <User key={user.id} user={user} />
     ));
+    if (displayedUsers.length === 0) {
+      return (
+        <p className="text-lg text-center text-neutral-600 mt-6">
+          Cannot add any friends.
+        </p>
+      );
+    }
     return displayedUsers;
   }
 
   function User({ user }) {
+    const [loader, setLoader] = useState(false);
+    const [added, setAdded] = useState(false);
     async function addToChat() {
+      setLoader(true);
       const response = await addUserToChat(
         authorizationData.token,
         chat.id,
@@ -35,9 +45,11 @@ function AddParticipantPopup({ chat, close }) {
       );
       if (response.ok) {
         console.log('user added');
+        setAdded(true);
       } else {
         console.log('error adding user');
       }
+      setLoader(false);
     }
 
     return (
@@ -67,10 +79,54 @@ function AddParticipantPopup({ chat, close }) {
             {user.username}
           </p>
           <button
+            disabled={loader || added}
             onClick={addToChat}
-            className="bg-sky-500 rounded-lg whitespace-nowrap text-white py-1 px-2 hover:bg-sky-700 transition"
+            className={`${
+              added ? 'bg-sky-600' : 'bg-sky-500 hover:bg-sky-700'
+            } w-[140px] rounded-lg whitespace-nowrap text-white py-1 px-2 flex items-center justify-center gap-2 transition `}
           >
-            Add to chat
+            {loader ? (
+              <div className="h-full w-full flex items-center justify-center ">
+                <svg
+                  aria-hidden="true"
+                  className="w-6 h-6 text-gray-300 animate-spin fill-white"
+                  viewBox="0 0 100 101"
+                  fill="none"
+                  xmlns="http://www.w3.org/2000/svg"
+                >
+                  <path
+                    d="M100 50.5908C100 78.2051 77.6142 100.591 50 100.591C22.3858 100.591 0 78.2051 0 50.5908C0 22.9766 22.3858 0.59082 50 0.59082C77.6142 0.59082 100 22.9766 100 50.5908ZM9.08144 50.5908C9.08144 73.1895 27.4013 91.5094 50 91.5094C72.5987 91.5094 90.9186 73.1895 90.9186 50.5908C90.9186 27.9921 72.5987 9.67226 50 9.67226C27.4013 9.67226 9.08144 27.9921 9.08144 50.5908Z"
+                    fill="currentColor"
+                  />
+                  <path
+                    d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z"
+                    fill="currentFill"
+                  />
+                </svg>
+              </div>
+            ) : added ? (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="fill-white w-5"
+                >
+                  <path d="M21,7L9,19L3.5,13.5L4.91,12.09L9,16.17L19.59,5.59L21,7Z" />
+                </svg>
+                Added
+              </>
+            ) : (
+              <>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 24 24"
+                  className="fill-white w-5"
+                >
+                  <path d="M15,14C12.33,14 7,15.33 7,18V20H23V18C23,15.33 17.67,14 15,14M6,10V7H4V10H1V12H4V15H6V12H9V10M15,12A4,4 0 0,0 19,8A4,4 0 0,0 15,4A4,4 0 0,0 11,8A4,4 0 0,0 15,12Z" />
+                </svg>
+                Add to chat
+              </>
+            )}
           </button>
         </div>
       </div>
