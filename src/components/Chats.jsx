e6ea99064257d734c0f233dd61699ec32da58740
Viktor Miric <viktorm47@gmail.com>
2023-10-31 23:02:42 +0100

    Styled chats in sidebar with timeago library

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -1,3 +1,7 @@
+import TimeAgo from 'javascript-time-ago';
+import en from 'javascript-time-ago/locale/en.json';
+TimeAgo.addDefaultLocale(en);
+
 function Chats({ chats, setMainDisplay }) {
   const userChats = chats.map((chat, index) => (
     <Chat key={index} chat={chat} setMainDisplay={setMainDisplay} />
@@ -31,9 +35,13 @@ function Chats({ chats, setMainDisplay }) {
 }
 
 function Chat({ chat, setMainDisplay }) {
+  const timeAgo = new TimeAgo('en-US');
+  const messageDate = new Date(chat.last_message.created_at);
+  const difference = timeAgo.format(messageDate);
+
   return (
     <div
-      className="bg-slate-100 m-1 rounded-sm border flex dark:hover:bg-slate-700 dark:bg-slate-800 dark:border-slate-700 border-slate-200 p-1 hover:bg-slate-200 cursor-pointer transition"
+      className="bg-slate-100 m-1 rounded-sm border flex dark:hover:bg-slate-700 dark:bg-slate-800 dark:border-slate-700 border-slate-200 p-1 relative hover:bg-slate-200 cursor-pointer transition"
       onClick={() => setMainDisplay(['chat', chat])}
     >
       {chat.image ? (
@@ -62,12 +70,19 @@ function Chat({ chat, setMainDisplay }) {
           <path d="M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M15.6,8.34C16.67,8.34 17.53,9.2 17.53,10.27C17.53,11.34 16.67,12.2 15.6,12.2A1.93,1.93 0 0,1 13.67,10.27C13.66,9.2 14.53,8.34 15.6,8.34M9.6,6.76C10.9,6.76 11.96,7.82 11.96,9.12C11.96,10.42 10.9,11.5 9.6,11.5C8.3,11.5 7.24,10.42 7.24,9.12C7.24,7.81 8.29,6.76 9.6,6.76M9.6,15.89V19.64C7.2,18.89 5.3,17.04 4.46,14.68C5.5,13.56 8.13,13 9.6,13C10.13,13 10.8,13.07 11.5,13.21C9.86,14.08 9.6,15.23 9.6,15.89M12,20C11.72,20 11.46,20 11.2,19.96V15.89C11.2,14.47 14.14,13.76 15.6,13.76C16.67,13.76 18.5,14.15 19.44,14.91C18.27,17.88 15.38,20 12,20Z" />
         </svg>
       )}
-
+      <p className="absolute top-0 right-0 text-xs text-neutral-600">
+        {difference}
+      </p>
       <div>
-        <h1 className="">{chat.name}</h1>
-        <h1>
-          {chat.last_message ? chat.last_message.body : 'No messages yet.'}
-        </h1>
+        <h1 className="font-semibold">{chat.name}</h1>
+        <div className="flex text-neutral-600">
+          {chat.last_message_author && (
+            <p className="font-semibold mr-2">{chat.last_message_author}:</p>
+          )}
+          <p>
+            {chat.last_message ? chat.last_message.body : 'No messages yet.'}
+          </p>
+        </div>
       </div>
     </div>
   );
